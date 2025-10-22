de7cf5aee9a18cfea377b75207955636c28a8257
brenolg <brenolgar@gmail.com>
2025-10-22 16:29:22 -0300

    max w

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-white">
+      <div className="min-h-screen bg-white max-w-screen">
         <Header setSigner={setSigner} signer={signer} />
         <main>
           <Routes>
